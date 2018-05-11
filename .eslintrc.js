3ef615ecf61fa2c7f770349ff967661d8a2babd0
Louis Tribouillard <ltribouillard@artprice.com>
2018-05-11 11:52:33 +0200

    . files

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "standard",
+    "parserOptions": {
+        "ecmaVersion": 6,
+    },
+    "rules": {
+        "semi": [2, "always"],
+        "comma-dangle": [2, "only-multiline"],
+    }
+};
