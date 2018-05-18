647baa598587fcf7c901975acf95f498b79b367a
Louis Tribouillard <ltribouillard@artprice.com>
2018-05-18 16:06:40 +0200

    bump dependencies + expose isClockwiseOrdered

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { MP5 } from './mp5.js';
 import { absHol } from './absHol.js';
-import { isSimple, orderClockwise, isConvex } from './utils.js';
+import { isSimple, orderClockwise, isConvex, isClockwiseOrdered } from './utils.js';
 
 export { absHol, MP5, isSimple, orderClockwise, isConvex };
 export default absHol;
