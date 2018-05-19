24e9712136837b7cfc3ef5ea3fafa413c1e0d816
Louis-T <louis.tribouillard@gmail.com>
2018-05-19 18:23:31 +0200

    Update index.js
    Export isClockwiseOrdered

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { MP5 } from './mp5.js';
 import { absHol } from './absHol.js';
 import { isSimple, orderClockwise, isConvex, isClockwiseOrdered } from './utils.js';
 
-export { absHol, MP5, isSimple, orderClockwise, isConvex };
+export { absHol, MP5, isSimple, orderClockwise, isConvex, isClockwiseOrdered };
 export default absHol;
